099eec1f0bd29831673f577ae3f9b54999930034
Inara Oenning <inaraoenning@gmail.com>
2025-08-22 11:59:47 -0300

    Formula Margem Lucro

--- Atividade03_ManipulacaoVariavel/src/Main.java
+++ Atividade03_ManipulacaoVariavel/src/Main.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,10 +9,10 @@ public class Main {
         String productName = "Meta Quest 3";
 
         //Quantidade
-        int quantityPurchased = 10;
+        int quantityPurchased = 2;
 
         //Valor Unitário
-        double unitPrice = 3500.00;
+        double unitPrice = 500.00;
 
         //Taxa
         double taxRate = 0.20;
@@ -18,29 +20,27 @@ public class Main {
         //Margem Lucro
         double profitMargin = 0.30;
 
-        var subTotal = unitPrice * quantityPurchased;
-        var subTotalTax = (subTotal * taxRate) + subTotal;
+        //Subtotal Bruto
+        double subTotal = unitPrice * quantityPurchased;
 
-        //Falta calcular a margem de lucro
-        //DecimalFormat para formatar os números como moeda
+        //Subtotal com Imposto
+        var subTotalTax = subTotal + (subTotal * taxRate);
 
-        System.out.println("Valor Total R$" + subTotal);
-        System.out.println("Valor Total com Imposto R$" + subTotalTax);
+        //Valor de Lucro (30% sobre subtotal com imposto)
+        double profitValue = subTotalTax * profitMargin;
 
+        //Preço final com lucro
+        double finalPrice = subTotalTax + profitValue;
 
+        //Formatação moeda
+        DecimalFormat df = new DecimalFormat("R$ #,##0.00");
 
+        System.out.println("Produto: " + productName);
+        System.out.println("Quantidade: " + quantityPurchased);
+        System.out.println("Subtotal: " + df.format(subTotal));
+        System.out.println("Com Imposto: " + df.format(subTotalTax));
+        System.out.println("Lucro (30%): " + df.format(profitValue));
+        System.out.println("Valor Final com Lucro: " + df.format(finalPrice));
 
-//        precoDeVenda = totalComImpostos * (1 + margemLucro);
-//        A parte 1 + 0.30 está fazendo o seguinte:
-//
-//        1 representa 100% do valor original (ou seja, sem lucro ainda)
-//
-//        0.30 representa 30% de lucro desejado
-//
-//        Então://
-//
-//        1 + 0.30 = 1.30 → significa 130% do valor original
-
-//        precoDeVenda = 100 * 1.30 = 130
     }
 }
